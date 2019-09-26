dff21bd5f6f37f32af655faec245b8ec54b965f1
Mr. Cherry <Thearness12@gmail.com>
2019-09-26 19:56:47 +0900

    Second

--- factorial.c
+++ factorial.c
@@ -1,2 +1,6 @@
 #include <stdio.h>
-int main() {    int num = 6;    int product = 720;    printf("Factorial of %d is %d\n", num, product); } 
+int main() {
+	int i;    int num = 6;    int product = 1;
+	for (i = 1; i <= num; i++) product *= i;
+	printf("Factorial of %d is %d\n", num, product);
+}
